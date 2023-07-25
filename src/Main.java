7acf2c43096e17408319ce57c2c59dc41c9ee0d2
jonatan-theisen <jonatan.a.theisen@gmail.com>
2023-07-24 23:36:43 -0300

    Printing the board

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
     }
 }
